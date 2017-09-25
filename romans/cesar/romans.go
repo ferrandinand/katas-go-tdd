6d9d60cf33d544d67613092871897e01c711a94b
CesarSaez <krizio@gmail.com>
2017-09-25 08:50:46 +0200

    renaming and cases up to 50

--- romans/cesar/romans.go
+++ romans/cesar/romans.go
@@ -5,29 +5,33 @@ import "strings"
 func main() {
 }
 
-func remaninder(number, divisor int) (restremainder, restquotion int) {
+func getremainderandquotient(number, divisor int) (remainder, quotient int) {
 
-	restremainder = number % divisor
-	restquotion = number / divisor
+	remainder = number % divisor
+	quotient = number / divisor
 
 	return
 }
-func Convert(number int) (romanresult string) {
-	//var romanresult string
 
-	var romanEquivalences = []struct {
-		arabic int    // input
-		roman  string // expected result
+//Convertarabictoroman is a func to arabic numbers to roman numbers
+func Convertarabictoroman(number int) (romanresult string) {
+	var romanequivalences = []struct {
+		arabic int
+		roman  string
 	}{
+		{arabic: 50, roman: "L"},
+		{arabic: 40, roman: "XL"},
+		{arabic: 10, roman: "X"},
+		{arabic: 9, roman: "IX"},
 		{arabic: 5, roman: "V"},
 		{arabic: 4, roman: "IV"},
 		{arabic: 1, roman: "I"},
 	}
 
-	for _, numbersmap := range romanEquivalences {
-		decimalremainder,numberofsymbols := remaninder(number, numbersmap.arabic)
+	for _, arabicromanmap := range romanequivalences {
+		decimalremainder, numberofsymbols := getremainderandquotient(number, arabicromanmap.arabic)
 		number = decimalremainder
-		romanresult += strings.Repeat(numbersmap.roman, numberofsymbols)
+		romanresult += strings.Repeat(arabicromanmap.roman, numberofsymbols)
 	}
 
 	return
--- romans/cesar/romans_test.go
+++ romans/cesar/romans_test.go
@@ -15,23 +15,44 @@ func TestRomans(t *testing.T) {
 
 var _ = Describe("Roman numbers", func() {
 	Context("Test of the first 6 numbers", func() {
-		It("convert 1 to roman", func() {
-			Expect(Convert(1)).Should(BeEquivalentTo("I"))
+		It("Converts the arabic number 1 to roman", func() {
+			Expect(Convertarabictoroman(1)).Should(BeEquivalentTo("I"))
 		})
-		It("convert 2 to roman", func() {
-			Expect(Convert(2)).Should(BeEquivalentTo("II"))
+		It("Converts the arabic number 2 to roman", func() {
+			Expect(Convertarabictoroman(2)).Should(BeEquivalentTo("II"))
 		})
-		It("convert 3 to roman", func() {
-			Expect(Convert(3)).Should(BeEquivalentTo("III"))
+		It("Converts the arabic number 3 to roman", func() {
+			Expect(Convertarabictoroman(3)).Should(BeEquivalentTo("III"))
 		})
-		It("convert 4 to roman", func() {
-			Expect(Convert(4)).Should(BeEquivalentTo("IV"))
+		It("Converts the arabic number 4 to roman", func() {
+			Expect(Convertarabictoroman(4)).Should(BeEquivalentTo("IV"))
 		})
-		It("convert 5 to roman", func() {
-			Expect(Convert(5)).Should(BeEquivalentTo("V"))
+		It("Converts the arabic number 5 to roman", func() {
+			Expect(Convertarabictoroman(5)).Should(BeEquivalentTo("V"))
 		})
-		It("convert 6 to roman", func() {
-			Expect(Convert(6)).Should(BeEquivalentTo("VI"))
+		It("Converts the arabic number 6 to roman", func() {
+			Expect(Convertarabictoroman(6)).Should(BeEquivalentTo("VI"))
+		})
+		It("Converts the arabic number 9 to roman", func() {
+			Expect(Convertarabictoroman(9)).Should(BeEquivalentTo("IX"))
+		})
+		It("Converts the arabic number 12 to roman", func() {
+			Expect(Convertarabictoroman(12)).Should(BeEquivalentTo("XII"))
+		})
+		It("Converts the arabic number 15 to roman", func() {
+			Expect(Convertarabictoroman(15)).Should(BeEquivalentTo("XV"))
+		})
+		It("Converts the arabic number 19 to roman", func() {
+			Expect(Convertarabictoroman(19)).Should(BeEquivalentTo("XIX"))
+		})
+		It("Converts the arabic number 40 to roman", func() {
+			Expect(Convertarabictoroman(40)).Should(BeEquivalentTo("XL"))
+		})
+		It("Converts the arabic number 41 to roman", func() {
+			Expect(Convertarabictoroman(41)).Should(BeEquivalentTo("XLI"))
+		})
+		It("Converts the arabic number 50 to roman", func() {
+			Expect(Convertarabictoroman(50)).Should(BeEquivalentTo("L"))
 		})
 	})
 })
