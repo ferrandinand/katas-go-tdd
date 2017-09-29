7de88eb4896135f8b1d100a2c9057c35a62ad6fc
Ferran Tomas <ferrandinand@gmail.com>
2017-09-29 08:46:17 +0200

    Add first fizz buzz functions

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "strconv"
 
 func fizzBuzz(max int) string {
 
@@ -34,28 +33,45 @@ func fill(n int) []int {
 	return maxnumber
 }
 
-func divisibleby3(n int) string {
+func divisibleby3(n int) bool {
 
 	if n%3 == 0 {
-		return "Fizz"
+		return true
 	}
-	return strconv.Itoa(n)
+	return false
+}
+
+func divisibleby5(n int) bool {
+
+	if n%5 == 0 {
+		return true
+	}
+	return false
 
 }
 
-func transformFizzOrBuzz(arrayofnumbers []int) []string {
-	var transformedArray []string
-	for i, value := range arrayofnumbers {
-		fmt.Println(value)
-		transformedArray[i] = strconv.Itoa(value)
+func divisibleby3and5(n int) bool {
 
+	if divisibleby3(n) && divisibleby5(n) {
+		return true
 	}
+	return false
 
-	// for i := 0; i < len(arrayofnumbers); i++ {
-
-	// 	transformedArray[i] = strconv.Itoa(arrayofnumbers[i])
-
-	// }
-
-	return transformedArray
 }
+
+//func transformFizzOrBuzz(arrayofnumbers []int) []string {
+// var transformedArray []string
+// 	for i, value := range arrayofnumbers {
+// 		fmt.Println(value)
+// 		transformedArray[i] = strconv.Itoa(value)
+
+// 	}
+
+// 	// for i := 0; i < len(arrayofnumbers); i++ {
+
+// 	// 	transformedArray[i] = strconv.Itoa(arrayofnumbers[i])
+
+// 	// }
+
+// 	return transformedArray
+// }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -11,13 +11,13 @@ func TestFizzBuzz(t *testing.T) {
 
 var _ = Describe("FizzBuzz", func() {
 	var (
-		Array            []int
-		transformedArray []string
+		Array []int
+		//transformedArray []string
 	)
 
 	BeforeEach(func() {
 		Array = fill(100)
-		transformedArray = transformFizzOrBuzz(Array)
+		//transformedArray = transformFizzOrBuzz(Array)
 	})
 
 	Context("Create list of 100", func() {
@@ -25,14 +25,27 @@ var _ = Describe("FizzBuzz", func() {
 			Expect(len(Array)).Should(BeEquivalentTo(100))
 		})
 	})
+
 	Context("Numbers divisible by 3 show Fizz", func() {
 		It("3 must return Fizz", func() {
-			Expect(divisibleby3(3)).To(Equal("Fizz"))
+			Expect(divisibleby3(3)).To(Equal(true))
 		})
 	})
-	Context("Check expected values", func() {
-		It("3 must return Fizz", func() {
-			Expect(len(transformedArray)).Should(BeEquivalentTo(100))
+
+	Context("Numbers divisible by 5 show Buzz", func() {
+		It("5 must return Fizz", func() {
+			Expect(divisibleby5(5)).To(Equal(true))
 		})
 	})
+
+	Context("Numbers divisible by 3 and 5 show FizzBuzz", func() {
+		It("15 must return FizzBuzz", func() {
+			Expect(divisibleby3and5(15)).To(Equal(true))
+		})
+	})
+	//Context("Check expected values", func() {
+	//	It("Contains Fizz Buzz FizzBuzz or integer", func() {
+	//		Expect((transformedArray)).Should()
+	//	})
+	//})
 })
